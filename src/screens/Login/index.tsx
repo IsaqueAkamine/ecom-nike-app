73674f7b320f067974b5fb21a17e4e99bcde0d95
Isaque Akamine <isaqueakamine@gmail.com>
2024-01-02 21:24:57 -0300

    build: added react navigation

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Container, Title} from './styles';
+
+const Login: React.FC = () => {
+  return (
+    <Container>
+      <Title>Login Screen</Title>
+    </Container>
+  );
+};
+
+export default Login;
---
+++ src/screens/Login/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+import {COLORS} from '../../constants/colors';
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  background-color: ${COLORS.backgroundColor};
+`;
+
+export const Title = styled.Text``;
